69c09d14969537765253b8731612aa41c1f0c248
ARTHUR FELIPE MARQUES <arthur_f_marques@estudante.sesisenai.org.br>
2023-10-03 20:09:39 -0300

    adicionado função de adicionar despesa

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const formulario = document.querySelector("form");
-var listaDespesas = [];
+
+if (!localStorage.getItem('listaDespesas')) {
+    localStorage.setItem('listaDespesas', '[]');
+}
+
+listaDespesas = JSON.parse(localStorage.getItem('listaDespesas'));
+
+
+function verificarDespesa() {
+
+    var despesa = document.getElementById("ttbDespesa").value;
+
+    despesa = Number(despesa);
+
+    if (despesa <= 0 || isNaN(despesa)) {
+
+        alert("Por favor inserir um valor de despesa válido");
+
+        return false;
+
+    }
+
+    return despesa;
+
+}
 
 formulario.addEventListener("submit", function(e) {
 
+    const despesa = verificarDespesa();
     
+    if (despesa) {
+
+        listaDespesas.push(despesa);
+
+        localStorage.setItem('listaDespesas', JSON.stringify(listaDespesas));
+
+    }
 
 })
